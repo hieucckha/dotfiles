3be5b1356b0159f813d7f24465e0e3dc677b519e
Hieu Nguyen <hieucckha@gmail.com>
2026-05-10 00:38:13 +0700

    update the colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,4 +1,17 @@
 -- INFO: colorscheme
 
-vim.pack.add({ "https://github.com/sainnhe/gruvbox-material" }, { confirm = false })
-vim.cmd.colorscheme("gruvbox-material")
+vim.pack.add({ "https://github.com/wincent/base16-nvim" }, { confirm = false })
+vim.cmd.colorscheme("gruvbox-dark-hard")
+vim.o.background = "dark"
+-- less visible window separator
+vim.api.nvim_set_hl(0, "WinSeparator", { fg = 1250067 })
+-- Make comments more prominent -- they are important.
+local bools = vim.api.nvim_get_hl(0, { name = "Boolean" })
+vim.api.nvim_set_hl(0, "Comment", bools)
+-- Make it clearly visible which argument we're at.
+local marked = vim.api.nvim_get_hl(0, { name = "PMenu" })
+vim.api.nvim_set_hl(
+	0,
+	"LspSignatureActiveParameter",
+	{ fg = marked.fg, bg = marked.bg, ctermfg = marked.ctermfg, ctermbg = marked.ctermbg, bold = true }
+)
